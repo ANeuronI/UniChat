51ba5c59e61497c7cf94ee8b9c28966bf3a299a0
Sudhanshu Suvasiya <159800404+ANeuronI@users.noreply.github.com>
2024-07-21 15:30:54 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,7 +29,7 @@ import Pagination from "@/components/shared/Pagination";
 
         <section className='mt-9 flex flex-col gap-10'>
           {result.posts.length === 0 ? (
-            <p className='no-result'>No threads found</p>
+            <p className='no-result'>No Posts found</p>
           ) : (
             <>
               {result.posts.map((post) => (
